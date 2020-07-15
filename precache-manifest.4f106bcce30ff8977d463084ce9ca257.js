a88a3ce055c35aa6f38be59de69dbc54b3298f32
Daniel <filind85@gmail.com>
2020-07-15 09:57:43 +0300

    Updates

---
+++ precache-manifest.4f106bcce30ff8977d463084ce9ca257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88a4aa8076f0b23b52e2620c673dd447",
+    "url": "/random-monster-tiny-demo-app/index.html"
+  },
+  {
+    "revision": "31b898dc69cc0ce62c81",
+    "url": "/random-monster-tiny-demo-app/static/css/main.f4bcc2b2.chunk.css"
+  },
+  {
+    "revision": "bb27650c9f5e6e6b07e2",
+    "url": "/random-monster-tiny-demo-app/static/js/2.70e63080.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-monster-tiny-demo-app/static/js/2.70e63080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31b898dc69cc0ce62c81",
+    "url": "/random-monster-tiny-demo-app/static/js/main.c401a9f1.chunk.js"
+  },
+  {
+    "revision": "299b157b1747ef8a7311",
+    "url": "/random-monster-tiny-demo-app/static/js/runtime-main.eaf364d1.js"
+  }
+]);
